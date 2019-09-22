39bb5eec6a289e0aca583cfd8ef02f5ded0e7a49
Kenneth Powers <ken@kenpowers.net>
2019-09-21 20:27:11 -0400

    feat(text): add new testament metadata

--- packages/text/src/meta.test.ts
+++ packages/text/src/meta.test.ts
@@ -1,9 +1,12 @@
 import { books } from './meta';
 
 test('meta info consistent', () => {
-  expect(books.length).toBe(39);
+  const otCount = 39;
+  const ntCount = 27;
+  expect(books.length).toBe(otCount + ntCount);
   books.forEach(book => {
     expect(book.verseCounts.length).toBe(book.chapters);
     expect(book.verseCounts.reduce((s, c) => s + c, 0)).toBe(book.verses);
   });
+  expect(books.reduce((s, b) => s + b.verses, 0)).toBe(31102);
 });
