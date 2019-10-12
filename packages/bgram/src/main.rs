a4aaf799f15b19adc6512f2d44916f843b3d1b4c
Kenneth Powers <ken@kenpowers.net>
2019-10-12 18:16:27 -0400

    refactor(bgram): use actix data instead of lazy_static

--- packages/bgram/src/main.rs
+++ packages/bgram/src/main.rs
@@ -1,10 +1,10 @@
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 use bgram::{BGramIndex, JsonVerse};
-use lazy_static::lazy_static;
 use log::info;
 use serde::Deserialize;
 use std::fs;
 use std::io::BufReader;
+use std::sync::Arc;
 use std::time::Instant;
 
 #[derive(Deserialize, Debug)]
@@ -17,48 +17,46 @@ struct SearchQuery {
     q: String,
 }
 
-lazy_static! {
-    static ref GRAM_INDEX: BGramIndex = {
-        let mut gi = BGramIndex::new();
-        let config = match envy::from_env::<Config>() {
-            Ok(config) => config,
-            Err(error) => panic!("{:?}", error),
-        };
-
-        info!("Loading translations from {:?}", config.translation_dir);
-        for entry in fs::read_dir(config.translation_dir).unwrap() {
-            let path = entry.unwrap().path();
-            if path.is_file() && path.extension().map(|s| s == "json").unwrap_or(false) {
-                let translation = path.file_stem().unwrap().to_string_lossy().to_string();
-                info!("Load translation {} from {:?}", translation, path);
-                let now = Instant::now();
-                let file = fs::File::open(path).unwrap();
-                let reader = BufReader::new(file);
-                let verses: Vec<JsonVerse> = serde_json::from_reader(reader).unwrap();
-                info!(
-                    "Read {} verses in {}ms",
-                    verses.len(),
-                    now.elapsed().as_millis()
-                );
-                let now = Instant::now();
-                for verse in &verses {
-                    gi.insert_verse(&translation, verse);
-                }
-                info!(
-                    "Indexed {} verses in {}ms",
-                    verses.len(),
-                    now.elapsed().as_millis()
-                );
-            }
-        }
-        gi
+fn make_index() -> Arc<BGramIndex> {
+    let mut gi = BGramIndex::new();
+    let config = match envy::from_env::<Config>() {
+        Ok(config) => config,
+        Err(error) => panic!("{:?}", error),
     };
+
+    info!("Loading translations from {:?}", config.translation_dir);
+    for entry in fs::read_dir(config.translation_dir).unwrap() {
+        let path = entry.unwrap().path();
+        if path.is_file() && path.extension().map(|s| s == "json").unwrap_or(false) {
+            let translation = path.file_stem().unwrap().to_string_lossy().to_string();
+            info!("Load translation {} from {:?}", translation, path);
+            let now = Instant::now();
+            let file = fs::File::open(path).unwrap();
+            let reader = BufReader::new(file);
+            let verses: Vec<JsonVerse> = serde_json::from_reader(reader).unwrap();
+            info!(
+                "Read {} verses in {}ms",
+                verses.len(),
+                now.elapsed().as_millis()
+            );
+            let now = Instant::now();
+            for verse in &verses {
+                gi.insert_verse(&translation, verse);
+            }
+            info!(
+                "Indexed {} verses in {}ms",
+                verses.len(),
+                now.elapsed().as_millis()
+            );
+        }
+    }
+    Arc::new(gi)
 }
 
-fn index(info: web::Query<SearchQuery>) -> HttpResponse {
+fn search(info: web::Query<SearchQuery>, index: web::Data<Arc<BGramIndex>>) -> HttpResponse {
     info!(r#"Searching for """{}""""#, info.q);
     let now = Instant::now();
-    let res = GRAM_INDEX.search(&info.q);
+    let res = index.search(&info.q);
     // format!("{} results in {}ms", res.len(), now.elapsed().as_millis())
     HttpResponse::Ok()
         .header("X-Response-Time", now.elapsed().as_millis() as u64)
@@ -69,10 +67,13 @@ fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "bgram=info,actix_web=info");
     env_logger::init();
 
-    HttpServer::new(|| {
+    let index = make_index();
+
+    HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(web::resource("/").to(index))
+            .data(index.clone())
+            .service(web::resource("/").to(search))
     })
     .bind("127.0.0.1:8080")?
     .run()
