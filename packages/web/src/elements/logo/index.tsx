c04c1662c4053ea48fda0e2f4759ebf8af99e281
Kenneth Powers <ken@kenpowers.net>
2019-12-26 12:43:03 -0500

    refactor(web): export typed useTheme

--- packages/web/src/elements/logo/index.tsx
+++ packages/web/src/elements/logo/index.tsx
@@ -2,18 +2,17 @@
 // See `SIL Open Font License.txt`
 
 import * as React from 'react';
-import { useTheme } from 'emotion-theming';
 import Light from './instantbible-logo-light.svg';
 import Dark from './instantbible-logo-dark.svg';
 import DarkAlt from './instantbible-logo-dark-alt.svg';
-import { Theme } from '../../util/theme';
+import { useTheme } from '../../util/theme';
 
 export type Props = {
   alt?: boolean;
 };
 
 const Logo: React.FunctionComponent<Props> = React.memo(({ alt }) => {
-  const { key: themeKey } = useTheme() as Theme;
+  const { key: themeKey } = useTheme();
 
   if (themeKey === 'dark' && alt) {
     return <DarkAlt />;
--- packages/web/src/util/theme.ts
+++ packages/web/src/util/theme.ts
@@ -1,3 +1,5 @@
+import { useTheme as useThemeFn } from 'emotion-theming';
+
 export const light = {
   key: 'light' as 'light' | 'dark',
   background: '#FAFAFB',
@@ -9,3 +11,5 @@ export const dark: Theme = {
   key: 'dark',
   background: '#13131A',
 };
+
+export const useTheme = useThemeFn as () => Theme;
