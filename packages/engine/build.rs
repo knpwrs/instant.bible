6dc9518bba99c590686548a1a3a9be39ed5a1ff0
Kenneth Powers <ken@kenpowers.net>
2019-12-31 13:38:23 -0500

    build(engine): rerun build when proto files change

--- packages/engine/build.rs
+++ packages/engine/build.rs
@@ -1,5 +1,9 @@
 fn main() {
+    println!("cargo:rerun-if-changed=../proto/data.proto");
+    println!("cargo:rerun-if-changed=../proto/service.proto");
+
     let mut prost = prost_build::Config::new();
+
     prost.type_attribute(
         ".instantbible.data.VerseKey",
         "#[derive(serde::Serialize, Hash, Eq, Copy)]",
@@ -8,6 +12,7 @@ fn main() {
         ".instantbible.service.Response",
         "#[derive(serde::Serialize)]",
     );
+
     prost
         .compile_protos(
             &["../proto/data.proto", "../proto/service.proto"],
