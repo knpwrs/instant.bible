6038efff38b3afeb34c750ad1081fdaa2540bfa8
Kenneth Powers <ken@kenpowers.net>
2020-02-12 07:52:58 -0500

    feat(crawler): implement basic commoncrawl crawler with bash

---
+++ packages/crawler/crawl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+BASE_URL="https://commoncrawl.s3.amazonaws.com"
+YEAR="$1"
+CRAWL="$2"
+PROC=${3:-2}
+OUTDIR="data/${YEAR}-${CRAWL}"
+
+cat books.multi.txt | sed 's|$|\\s?\\d{1,3}:\\d{1,3}|g' > verse.regex.build
+cat books.single.txt | sed 's|$|\\s?(?:\\d{1,3}:)?\\d{1,3}|g' >> verse.regex.build
+
+mkdir -p $OUTDIR
+
+curl -s "$BASE_URL/crawl-data/CC-MAIN-$YEAR-$CRAWL/wet.paths.gz" \
+  | zcat \
+  | xargs -n1 -P $PROC -I% ./_crawl.sh $BASE_URL % $OUTDIR
