0488242095d56f77c56d439791244b5dde16fb49
Kenneth Powers <ken@kenpowers.net>
2020-02-13 11:38:26 -0500

    refactor(crawler): no more need for LC_ALL

--- packages/crawler/collate.sh
+++ packages/crawler/collate.sh
@@ -4,8 +4,6 @@ YEAR="$1"
 CRAWL="$2"
 OUTDIR="data/${YEAR}-${CRAWL}"
 
-export LC_ALL='C'
-
 ls $OUTDIR \
   | xargs -I% cat $OUTDIR/% \
   | sed 's|song[^0-9]*|Song of Solomon |ig;s|Revelations|Revelation|ig;s|Psalm |Psalms |ig;s|^3|Third|ig;s|^III|Third|ig;s|^2|Second|ig;s|^II|Second|ig;s|^1|First|ig;s|^I|First|ig' \
