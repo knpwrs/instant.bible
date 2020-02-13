0b728ee64a04af45afb11042beb6f1f547990872
Kenneth Powers <ken@kenpowers.net>
2020-02-12 21:15:55 -0500

    fix(crawler): fix Song of Solomon collation

--- packages/crawler/collate.sh
+++ packages/crawler/collate.sh
@@ -8,7 +8,7 @@ export LC_ALL='C'
 
 ls $OUTDIR \
   | xargs -I% cat $OUTDIR/% \
-  | sed 's|song.*$|Song of Solomon|ig;s|Revelations|Revelation|ig;s|Psalm |Psalms |ig;s|^3|Third|ig;s|^III|Third|ig;s|^2|Second|ig;s|^II|Second|ig;s|^1|First|ig;s|^I|First|ig' \
+  | sed 's|song[^0-9]*|Song of Solomon |ig;s|Revelations|Revelation|ig;s|Psalm |Psalms |ig;s|^3|Third|ig;s|^III|Third|ig;s|^2|Second|ig;s|^II|Second|ig;s|^1|First|ig;s|^I|First|ig' \
   | tr '[:lower:]' '[:upper:]' \
   | sort \
   | uniq -c > "$OUTDIR.txt"
