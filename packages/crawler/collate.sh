d1b1c052fed09fede4b1de4f78b39203f412a045
Kenneth Powers <ken@kenpowers.net>
2020-02-26 18:11:10 -0500

    refactor(crawler): split collate into cat and collate

--- packages/crawler/collate.sh
+++ packages/crawler/collate.sh
@@ -6,9 +6,6 @@ OUTDIR="data/${YEAR}-${CRAWL}"
 
 export LC_ALL='C'
 
-ls $OUTDIR \
-  | xargs -I% cat $OUTDIR/% \
-  | sed 's|song[^0-9]*|Song of Solomon |ig;s|Revelations|Revelation|ig;s|Psalm |Psalms |ig;s|^3|Third|ig;s|^III|Third|ig;s|^2|Second|ig;s|^II|Second|ig;s|^1|First|ig;s|^I |First |ig' \
-  | tr '[:lower:]' '[:upper:]' \
-  | sort \
-  | uniq -c > "$OUTDIR.txt"
+./cat.sh $YEAR $CRAWL \
+  | sort
+  | uniq -c
