a90e24b0cc86ba0d37a59b7db1bc59358b9a70b3
Kenneth Powers <ken@kenpowers.net>
2020-03-06 07:24:50 -0500

    feat(engine): popularity bytes

--- packages/engine/src/proto.rs
+++ packages/engine/src/proto.rs
@@ -1,5 +1,5 @@
 pub mod data {
-    use anyhow::{Context, Result};
+    use anyhow::{anyhow, Context, Result};
     use prost::Message;
 
     include!(concat!(env!("OUT_DIR"), "/instantbible.data.rs"));
@@ -16,6 +16,7 @@ pub mod data {
             v.extend(&(self.verse as u8).to_be_bytes());
             v
         }
+
         pub fn from_be_bytes(bytes: &[u8]) -> Self {
             let book = u8::from_be_bytes([bytes[0]]) as i32;
             let chapter = u8::from_be_bytes([bytes[1]]) as u32;
@@ -24,6 +25,80 @@ pub mod data {
                 book,
                 chapter,
                 verse,
+            }
+        }
+    }
+
+    impl Book {
+        pub fn from_string(name: &str) -> Result<Self> {
+            match name {
+                "GENESIS" => Ok(Self::Genesis),
+                "EXODUS" => Ok(Self::Exodus),
+                "LEVITICUS" => Ok(Self::Leviticus),
+                "NUMBERS" => Ok(Self::Numbers),
+                "DEUTERONOMY" => Ok(Self::Deuteronomy),
+                "JOSHUA" => Ok(Self::Joshua),
+                "JUDGES" => Ok(Self::Judges),
+                "RUTH" => Ok(Self::Ruth),
+                "FIRST SAMUEL" => Ok(Self::FirstSamuel),
+                "SECOND SAMUEL" => Ok(Self::SecondSamuel),
+                "FIRST KINGS" => Ok(Self::FirstKings),
+                "SECOND KINGS" => Ok(Self::SecondKings),
+                "FIRST CHRONICLES" => Ok(Self::FirstChronicles),
+                "SECOND CHRONICLES" => Ok(Self::SecondChronicles),
+                "EZRA" => Ok(Self::Ezra),
+                "NEHEMIAH" => Ok(Self::Nehemiah),
+                "ESTHER" => Ok(Self::Esther),
+                "JOB" => Ok(Self::Job),
+                "PSALMS" => Ok(Self::Psalms),
+                "PROVERBS" => Ok(Self::Proverbs),
+                "ECCLESIASTES" => Ok(Self::Ecclesiastes),
+                "SONG OF SOLOMON" => Ok(Self::SongOfSolomon),
+                "ISAIAH" => Ok(Self::Isaiah),
+                "JEREMIAH" => Ok(Self::Jeremiah),
+                "LAMENTATIONS" => Ok(Self::Lamentations),
+                "EZEKIEL" => Ok(Self::Ezekiel),
+                "DANIEL" => Ok(Self::Daniel),
+                "HOSEA" => Ok(Self::Hosea),
+                "JOEL" => Ok(Self::Joel),
+                "AMOS" => Ok(Self::Amos),
+                "OBADIAH" => Ok(Self::Obadiah),
+                "JONAH" => Ok(Self::Jonah),
+                "MICAH" => Ok(Self::Micah),
+                "NAHUM" => Ok(Self::Nahum),
+                "HABAKKUK" => Ok(Self::Habakkuk),
+                "ZEPHANIAH" => Ok(Self::Zephaniah),
+                "HAGGAI" => Ok(Self::Haggai),
+                "ZECHARIAH" => Ok(Self::Zechariah),
+                "MALACHI" => Ok(Self::Malachi),
+                "MATTHEW" => Ok(Self::Matthew),
+                "MARK" => Ok(Self::Mark),
+                "LUKE" => Ok(Self::Luke),
+                "JOHN" => Ok(Self::John),
+                "ACTS" => Ok(Self::Acts),
+                "ROMANS" => Ok(Self::Romans),
+                "FIRST CORINTHIANS" => Ok(Self::FirstCorinthians),
+                "SECOND CORINTHIANS" => Ok(Self::SecondCorinthians),
+                "GALATIANS" => Ok(Self::Galatians),
+                "EPHESIANS" => Ok(Self::Ephesians),
+                "PHILIPPIANS" => Ok(Self::Philippians),
+                "COLOSSIANS" => Ok(Self::Colossians),
+                "FIRST THESSALONIANS" => Ok(Self::FirstThessalonians),
+                "SECOND THESSALONIANS" => Ok(Self::SecondThessalonians),
+                "FIRST TIMOTHY" => Ok(Self::FirstTimothy),
+                "SECOND TIMOTHY" => Ok(Self::SecondTimothy),
+                "TITUS" => Ok(Self::Titus),
+                "PHILEMON" => Ok(Self::Philemon),
+                "HEBREWS" => Ok(Self::Hebrews),
+                "JAMES" => Ok(Self::James),
+                "FIRST PETER" => Ok(Self::FirstPeter),
+                "SECOND PETER" => Ok(Self::SecondPeter),
+                "FIRST JOHN" => Ok(Self::FirstJohn),
+                "SECOND JOHN" => Ok(Self::SecondJohn),
+                "THIRD JOHN" => Ok(Self::ThirdJohn),
+                "JUDE" => Ok(Self::Jude),
+                "REVELATION" => Ok(Self::Revelation),
+                _ => Err(anyhow!("Invalid string for book")),
             }
         }
     }
