19d686a9653b33efb5ab9275afb77e8af573558a
Kenneth Powers <ken@kenpowers.net>
2020-05-10 13:40:34 -0400

    refactor(web): only activate sentry in production

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -3,9 +3,11 @@ import { render } from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import Root from './root';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
+}
 
 const root = document.getElementById('root');
 
