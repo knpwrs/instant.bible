05fd34d0643dd887bef10fda31854d43be953e5e
Kenneth Powers <ken@kenpowers.net>
2020-05-10 14:54:37 -0400

    feat(web): add translated title to settings button

--- packages/web/src/app/settings/index.tsx
+++ packages/web/src/app/settings/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { I18n } from '@lingui/react';
+import { t } from '@lingui/macro';
 import { css } from '@emotion/core';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import SettingsModal from './modal';
@@ -18,26 +20,31 @@ export default React.memo(() => {
   );
 
   return (
-    <>
-      <IconButton
-        icon={faCog}
-        css={css`
-          position: fixed;
-          top: 1em;
-          right: 1em;
-          opacity: 0.5;
-          font-size: 1.3em;
-          background: none;
-          border: none;
-          cursor: pointer;
-          outline: none;
-          &:hover {
-            opacity: 1;
-          }
-        `}
-        onClick={handleToggleOpen}
-      />
-      {open ? <SettingsModal onClose={handleToggleOpen} /> : null}
-    </>
+    <I18n>
+      {({ i18n }) => (
+        <>
+          <IconButton
+            icon={faCog}
+            css={css`
+              position: fixed;
+              top: 1em;
+              right: 1em;
+              opacity: 0.5;
+              font-size: 1.3em;
+              background: none;
+              border: none;
+              cursor: pointer;
+              outline: none;
+              &:hover {
+                opacity: 1;
+              }
+            `}
+            onClick={handleToggleOpen}
+            title={i18n._(t`Settings`)}
+          />
+          {open ? <SettingsModal onClose={handleToggleOpen} /> : null}
+        </>
+      )}
+    </I18n>
   );
 });
