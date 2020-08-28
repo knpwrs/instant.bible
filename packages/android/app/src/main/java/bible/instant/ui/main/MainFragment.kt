668441bc3055cc4deddc224fff5692d1ba768b2e
Kenneth Powers <ken@kenpowers.net>
2020-08-27 20:06:07 -0400

    style(android): clean up unused imports

--- packages/android/app/src/main/java/bible/instant/ui/main/MainFragment.kt
+++ packages/android/app/src/main/java/bible/instant/ui/main/MainFragment.kt
@@ -1,13 +1,10 @@
 package bible.instant.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
