37804d37e6cbaefae4edce9f7bcb505be11edebd
Kenneth Powers <ken@kenpowers.net>
2020-08-27 20:04:58 -0400

    feat(android): add translation switching

--- packages/android/app/src/main/java/bible/instant/Util.kt
+++ packages/android/app/src/main/java/bible/instant/Util.kt
@@ -1,6 +1,84 @@
 package bible.instant
 
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+import instantbible.data.Data
 
-class VerseResultViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+fun getTranslationLabel(translation: Int): String {
+    return when (translation) {
+        Data.Translation.BSB_VALUE -> "BSB"
+        Data.Translation.KJV_VALUE -> "KJV"
+        Data.Translation.NET_VALUE -> "NET"
+        else -> "UNRC"
+    }
+}
+
+fun getBookName(book: Data.Book): String {
+    return when (book) {
+        Data.Book.GENESIS -> "Genesis"
+        Data.Book.EXODUS -> "Exodus"
+        Data.Book.LEVITICUS -> "Leviticus"
+        Data.Book.NUMBERS -> "Numbers"
+        Data.Book.DEUTERONOMY -> "Deuteronomy"
+        Data.Book.JOSHUA -> "Joshua"
+        Data.Book.JUDGES -> "Judges"
+        Data.Book.RUTH -> "Ruth"
+        Data.Book.FIRST_SAMUEL -> "1 Samuel"
+        Data.Book.SECOND_SAMUEL -> "2 Samuel"
+        Data.Book.FIRST_KINGS -> "1 Kings"
+        Data.Book.SECOND_KINGS -> "2 Kings"
+        Data.Book.FIRST_CHRONICLES -> "1 Chronicles"
+        Data.Book.SECOND_CHRONICLES -> "2 Chronicles"
+        Data.Book.EZRA -> "Ezra"
+        Data.Book.NEHEMIAH -> "Nehemiah"
+        Data.Book.ESTHER -> "Esther"
+        Data.Book.JOB -> "Job"
+        Data.Book.PSALMS -> "Psalms"
+        Data.Book.PROVERBS -> "Proverbs"
+        Data.Book.ECCLESIASTES -> "Ecclesiastes"
+        Data.Book.SONG_OF_SOLOMON -> "Song of Solomon"
+        Data.Book.ISAIAH -> "Isaiah"
+        Data.Book.JEREMIAH -> "Jeremiah"
+        Data.Book.LAMENTATIONS -> "Lamentations"
+        Data.Book.EZEKIEL -> "Ezekiel"
+        Data.Book.DANIEL -> "Daniel"
+        Data.Book.HOSEA -> "Hosea"
+        Data.Book.JOEL -> "Joel"
+        Data.Book.AMOS -> "Amos"
+        Data.Book.OBADIAH -> "Obadiah"
+        Data.Book.JONAH -> "Jonah"
+        Data.Book.MICAH -> "Micha"
+        Data.Book.NAHUM -> "Nahum"
+        Data.Book.HABAKKUK -> "Habakkuk"
+        Data.Book.ZEPHANIAH -> "Zephaniah"
+        Data.Book.HAGGAI -> "Haggai"
+        Data.Book.ZECHARIAH -> "Zechariah"
+        Data.Book.MALACHI -> "Malachi"
+        Data.Book.MATTHEW -> "Matthew"
+        Data.Book.MARK -> "Mark"
+        Data.Book.LUKE -> "Luke"
+        Data.Book.JOHN -> "John"
+        Data.Book.ACTS -> "Acts"
+        Data.Book.ROMANS -> "Romans"
+        Data.Book.FIRST_CORINTHIANS -> "1 Corinthians"
+        Data.Book.SECOND_CORINTHIANS -> "2 Corinthians"
+        Data.Book.GALATIANS -> "Galatians"
+        Data.Book.EPHESIANS -> "Ephesians"
+        Data.Book.PHILIPPIANS -> "Philippians"
+        Data.Book.COLOSSIANS -> "Colossians"
+        Data.Book.FIRST_THESSALONIANS -> "1 Thessalonians"
+        Data.Book.SECOND_THESSALONIANS -> "2 Thessalonians"
+        Data.Book.FIRST_TIMOTHY -> "1 Timothy"
+        Data.Book.SECOND_TIMOTHY -> "2 Timothy"
+        Data.Book.TITUS -> "Titus"
+        Data.Book.PHILEMON -> "Philemon"
+        Data.Book.HEBREWS -> "Hebrews"
+        Data.Book.JAMES -> "James"
+        Data.Book.FIRST_PETER -> "1 Peter"
+        Data.Book.SECOND_PETER -> "2 Peter"
+        Data.Book.FIRST_JOHN -> "1 John"
+        Data.Book.SECOND_JOHN -> "2 John"
+        Data.Book.THIRD_JOHN -> "3 John"
+        Data.Book.JUDE -> "Jude"
+        Data.Book.REVELATION -> "Revelation"
+        Data.Book.UNRECOGNIZED -> "Unrecognized"
+    }
+}
--- packages/android/app/src/main/java/bible/instant/ui/main/VerseResultAdapter.kt
+++ packages/android/app/src/main/java/bible/instant/ui/main/VerseResultAdapter.kt
@@ -1,13 +1,23 @@
 package bible.instant.ui.main
 
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import bible.instant.R
-import bible.instant.VerseResultViewHolder
+import bible.instant.getBookName
+import bible.instant.getTranslationLabel
+import instantbible.data.Data
 import instantbible.service.Service
+
+class VerseResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val verseTitle: TextView = itemView.findViewById(R.id.verse_title)
+    val verseText: TextView = itemView.findViewById(R.id.verse_text)
+    val translationsHolder: LinearLayout = itemView.findViewById(R.id.translations)
+}
 
 class VerseResultAdapter : RecyclerView.Adapter<VerseResultViewHolder>() {
     var data = listOf<Service.Response.VerseResult>()
@@ -20,12 +30,31 @@ class VerseResultAdapter : RecyclerView.Adapter<VerseResultViewHolder>() {
 
     override fun onBindViewHolder(holder: VerseResultViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.getText(item.topTranslationValue)
+
+        holder.verseTitle.text = "${getBookName(item.key.book)} ${item.key.chapter}:${item.key.verse}"
+        holder.verseText.text = item.getText(item.topTranslationValue)
+
+        for (t in 0 until holder.translationsHolder.childCount) {
+            val btn = holder.translationsHolder.getChildAt(t)
+            btn.setOnClickListener {
+                holder.verseText.text = item.getText(btn.getTag(R.string.translation_tag) as Int)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VerseResultViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.verse_result_view, parent, false) as TextView
+        val view = layoutInflater.inflate(R.layout.verse_result_view, parent, false)
+        val translationsHolder: LinearLayout = view.findViewById(R.id.translations)
+
+        for (t in 0 until Data.Translation.TOTAL_VALUE) {
+            val btn = Button(view.context)
+            btn.text = getTranslationLabel(t)
+            btn.setTag(R.string.translation_tag, t)
+            translationsHolder.addView(btn)
+        }
+
         return VerseResultViewHolder(view)
     }
 }
+
