844ff952650a12b2e5bcef821e0e172f4e57d087
Kenneth Powers <ken@kenpowers.net>
2020-09-26 20:38:50 -0400

    feat(ios): add missing verse design

--- packages/ios/instant.bible/IbVerseResultView.swift
+++ packages/ios/instant.bible/IbVerseResultView.swift
@@ -49,8 +49,28 @@ struct IbVerseResultView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text(self.model.getTitle()).bold()
-            IbHighlighter(text: self.model.getBody(), words: self.model.result.highlights)
-                .padding(.vertical)
+            if self.model.getBody().isEmpty {
+                if #available(iOS 14.0, *) {
+                    (
+                        Text("\(Image("fa-dove-solid")) This verse is not available in the ")
+                            + Text(IbTranslationNameMap[self.model.selectedTranslation]!)
+                            + Text(" translation")
+                    )
+                        .foregroundColor(Color.ibTextSecondary)
+                        .padding(.vertical)
+                } else {
+                    (
+                        Text("This verse is not available in the ")
+                        + Text(IbTranslationNameMap[self.model.selectedTranslation]!)
+                        + Text(" translation")
+                    )
+                        .foregroundColor(Color.ibTextSecondary)
+                        .padding(.vertical)
+                }
+            } else {
+                IbHighlighter(text: self.model.getBody(), words: self.model.result.highlights)
+                    .padding(.vertical)
+            }
             HStack {
                 ForEach(translationKeys, id: \.self) { key in
                     IbTranslationButton(translation: key, selectedTranslation: self.model.selectedTranslation) {
@@ -80,20 +100,36 @@ struct IbVerseResultView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
             Rectangle().fill(Color.ibBackground)
-            IbVerseResultView(result: .with {
-                $0.key = .with {
-                    $0.book = .john
-                    $0.chapter = 3
-                    $0.verse = 16
-                }
-                $0.topTranslation = .kjv
-                $0.text = [
-                    "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life.",
-                    "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.",
-                    "For this is the way God loved the world: He gave his one and only Son, so that everyone who believes in him will not perish but have eternal life.",
-                ]
-                $0.highlights = ["HIS", "LIFE"]
-            })
+            VStack {
+                IbVerseResultView(result: .with {
+                    $0.key = .with {
+                        $0.book = .john
+                        $0.chapter = 3
+                        $0.verse = 16
+                    }
+                    $0.topTranslation = .kjv
+                    $0.text = [
+                        "For God so loved the world that He gave His one and only Son, that everyone who believes in Him shall not perish but have eternal life.",
+                        "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.",
+                        "For this is the way God loved the world: He gave his one and only Son, so that everyone who believes in him will not perish but have eternal life.",
+                    ]
+                    $0.highlights = ["HIS", "LIFE"]
+                })
+                IbVerseResultView(result: .with {
+                    $0.key = .with {
+                        $0.book = .matthew
+                        $0.chapter = 21
+                        $0.verse = 17
+                    }
+                    $0.topTranslation = .net
+                    $0.text = [
+                        "Howbeit this kind goeth not out but by prayer and fasting.",
+                        "",
+                        "",
+                    ]
+                    $0.highlights = ["PRAYER", "FASTING"]
+                })
+            }
         }
     }
 }
