a6ece78e9b65d507b8e1ad9bfec14249758e0cbc
Kenneth Powers <ken@kenpowers.net>
2020-09-27 16:47:58 -0400

    feat(ios): add open source software disclosure

--- packages/ios/instant.bible/IbSettingsSheet.swift
+++ packages/ios/instant.bible/IbSettingsSheet.swift
@@ -1,44 +1,71 @@
 import SwiftUI
 
-
 struct IbSettingsSheet: View {
     @Environment(\.presentationMode) var presentationMode
     @Binding var offlineEnabled: Bool
     @Binding var downloadProgress: Double
     @Binding var downloading: Bool
     @Binding var downloadSize: Int64
+    @State private var showingLicenses: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Settings").font(.largeTitle)
-            Toggle(isOn: self.$offlineEnabled) {
-                Text("Enable Offline Mode").font(.headline)
-            }
-            .disabled(self.downloading)
-            Text("Store the search index locally and search without making web requests (i.e., make instant.bible even ") + Text("instanter").italic() + Text(").")
-            if (self.downloadSize > 0) {
-                Text("Download size: \(ByteCountFormatter.string(fromByteCount: self.downloadSize, countStyle: .file)))")
-                    .padding(.top)
-            }
-            if (self.offlineEnabled) {
-                IbProgressBar(value: self.$downloadProgress)
-                    .padding(.top)
-            }
-            HStack {
-                Spacer()
-                Button("Close") {
-                    self.presentationMode.wrappedValue.dismiss()
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Settings").font(.largeTitle)
+                
+                Toggle(isOn: self.$offlineEnabled) {
+                    Text("Enable Offline Mode").font(.headline)
                 }
-                .foregroundColor(Color.ibText)
-                .padding()
+                .disabled(self.downloading)
+                
+                (
+                    Text("Store the search index locally and search without making web requests (i.e., make instant.bible even ")
+                        + Text("instanter").italic()
+                        + Text(").")
+                ).padding(.vertical)
+                
+                if (self.downloadSize > 0) {
+                    Text("Download size: \(ByteCountFormatter.string(fromByteCount: self.downloadSize, countStyle: .file)))")
+                        .padding(.top)
+                }
+                
+                if (self.offlineEnabled) {
+                    IbProgressBar(value: self.$downloadProgress)
+                        .padding(.top)
+                }
+                
+                Button("Open Source Software Disclosure") {
+                    self.showingLicenses.toggle()
+                }
+                    .padding(.vertical)
+                
+                if (self.showingLicenses) {
+                    Text("instant.bible uses the following open source software:")
+                        .padding(.vertical)
+                    Text(" · Alamofire (MIT) https://github.com/Alamofire/Alamofire")
+                    Text(" · SwiftUI-Introspect (MIT) https://github.com/siteline/SwiftUI-Introspect")
+                    Text(" · sentry-cocoa (MIT) https://github.com/getsentry/sentry-cocoa")
+                    Text(" · swift-protobuf (Apache 2.0) https://github.com/apple/swift-protobuf")
+                    Text(" · fst (MIT) https://github.com/burntsushi/fst")
+                    Text(" · Font Awesome 5 (CC BY 4.0) https://fontawesome.com/license/free")
+                }
+                
+                HStack {
+                    Spacer()
+                    Button("Close") {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                    .foregroundColor(Color.ibText)
+                    .padding()
+                    Spacer()
+                }
+                .padding(.vertical, 20)
                 Spacer()
             }
-            .padding(.vertical, 20)
-            Spacer()
+            .padding()
         }
-        .padding()
-        .background(Color.ibBackground.edgesIgnoringSafeArea(.all))
-        .transition(.slide)
+            .background(Color.ibBackground.edgesIgnoringSafeArea(.all))
+            .transition(.slide)
     }
 }
 
