42afd53c5877b667f768d037a76b2f5a3e7df0d0
Kenneth Powers <ken@kenpowers.net>
2020-10-06 19:42:40 -0400

    chore(web): consolidate typings

--- packages/web/src/opt/custom.d.ts
+++ packages/web/src/opt/custom.d.ts
@@ -4,3 +4,12 @@ declare module '*.svg' {
   const value: SvgrComponent;
   export default value;
 }
+
+declare module 'worker-loader!*' {
+  // You need to change `Worker`, if you specified a different value for the `workerType` option
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+
+  export default WebpackWorker;
+}
